ec11c0cca4d9a404694076051f132beadd6064b0
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-04-13 22:03:34 +0900

    mysite 게시판 구현

--- mysite/src/main/java/com/javaex/controller/BoardServlet.java
+++ mysite/src/main/java/com/javaex/controller/BoardServlet.java
@@ -1,6 +1,11 @@
 package com.javaex.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -10,7 +15,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import com.javaex.dao.BoardDao;
+import com.javaex.dao.BoardDaoImpl;
 import com.javaex.util.WebUtil;
 import com.javaex.vo.BoardVo;
 import com.javaex.vo.UserVo;
@@ -18,30 +28,78 @@ import com.javaex.vo.UserVo;
 @WebServlet("/board")
 public class BoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	// 파일 업로드
+	private static final String CHARSET = "utf-8";
+    private static final String ATTACHES_DIR = "C:\\Users\\hhh73\\StudioProjects\\JSP_Servlet\\mysite\\src\\main\\webapp\\files";
+    private static final int LIMIT_SIZE_BYTES = 1024 * 1024;
+
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		String actionName = request.getParameter("a");
 		System.out.println("board:" + actionName);
 
 		if ("list".equals(actionName)) {
-			// 리스트 가져오기
-			BoardDao dao = new BoardDao();
-			List<BoardVo> list = dao.getList();
-
+//			// 리스트 가져오기
+//			BoardDao dao = new BoardDaoImpl();
+//			List<BoardVo> list = dao.getList();
+//
+//			System.out.println(list.toString());
+//
+//			// 리스트 화면에 보내기
+//			request.setAttribute("list", list);
+//			WebUtil.forward(request, response, "/WEB-INF/views/board/list.jsp");
+			
+			BoardDao dao = new BoardDaoImpl();
+			int cnt = dao.getList().size();
+			
+			// 한 페이지에 출력될 리스트 개수
+			int pageSize = 10;
+			
+			// 현재 페이지 정보 설정
+			String pageNum = request.getParameter("pageNum");
+			if(pageNum == null) {
+				pageNum = "1";
+			}
+			System.out.println(pageNum);
+			
+			
+			// 각 페이지의 첫행번호를 계산
+			int currentPage = Integer.parseInt(pageNum);
+			int startRow = (currentPage-1) * pageSize + 1;
+			
+			List<BoardVo> list = dao.getBoardList(startRow, pageSize * Integer.parseInt(pageNum));
 			System.out.println(list.toString());
-
-			// 리스트 화면에 보내기
+			
 			request.setAttribute("list", list);
+			if(cnt % pageSize == 0) {
+				request.setAttribute("totalPage", cnt / pageSize);
+			}
+			else {
+				request.setAttribute("totalPage", cnt / pageSize + 1);
+			}
+			request.setAttribute("currentPage", currentPage);
+			
 			WebUtil.forward(request, response, "/WEB-INF/views/board/list.jsp");
 		} else if ("read".equals(actionName)) {
 			// 게시물 가져오기
 			int no = Integer.parseInt(request.getParameter("no"));
-			BoardDao dao = new BoardDao();
+			BoardDao dao = new BoardDaoImpl();
 			BoardVo boardVo = dao.getBoard(no);
 
 			System.out.println(boardVo.toString());
+			
+			if(request.getParameter("hit") != null) {
+				// 게시물 조회수 올리기
+				int hit = Integer.parseInt(request.getParameter("hit"));
+				BoardVo vo = new BoardVo(no, hit);
+				BoardDao dao2 = new BoardDaoImpl();
+				
+				dao2.update_hit(vo);
+			}
 
 			// 게시물 화면에 보내기
 			request.setAttribute("boardVo", boardVo);
@@ -49,7 +107,7 @@ public class BoardServlet extends HttpServlet {
 		} else if ("modifyform".equals(actionName)) {
 			// 게시물 가져오기
 			int no = Integer.parseInt(request.getParameter("no"));
-			BoardDao dao = new BoardDao();
+			BoardDao dao = new BoardDaoImpl();
 			BoardVo boardVo = dao.getBoard(no);
 
 			// 게시물 화면에 보내기
@@ -62,7 +120,7 @@ public class BoardServlet extends HttpServlet {
 			int no = Integer.parseInt(request.getParameter("no"));
 			
 			BoardVo vo = new BoardVo(no, title, content);
-			BoardDao dao = new BoardDao();
+			BoardDao dao = new BoardDaoImpl();
 			
 			dao.update(vo);
 			
@@ -77,18 +135,80 @@ public class BoardServlet extends HttpServlet {
 			}
 
 		} else if ("write".equals(actionName)) {
-			UserVo authUser = getAuthUser(request);
+			// 파일 업로드
+			PrintWriter out = response.getWriter();
+			 
+	        File attachesDir = new File(ATTACHES_DIR);
+	 
+	 
+	        DiskFileItemFactory fileItemFactory = new DiskFileItemFactory(); // 업로드의 데이터를 메모리에 임시로 저장할 크기를 만듦
+	        fileItemFactory.setRepository(attachesDir); // 업로드 저장파일을 임시로 저장할 폴더 생성
+	        fileItemFactory.setSizeThreshold(LIMIT_SIZE_BYTES); // 업로드 메모리 크기를 지정
+	        ServletFileUpload fileUpload = new ServletFileUpload(fileItemFactory); // 파일 업로드 핸들러
+	        
+	        
+	        String[] file = new String[2];
+	        int i = 0;
+	        
+	        String title = null, content = null;
 
-			String title = request.getParameter("title");
-			String content = request.getParameter("content");
+	        try {
+	        	System.out.println("성공!");
+	            List<FileItem> items = fileUpload.parseRequest(request);
+	            for (FileItem item : items) {
+	                if (item.isFormField()) { // input의 text와 같은 속성
+	                    System.out.printf("파라미터 명 : %s, 파라미터 값 :  %s \n", item.getFieldName(), item.getString(CHARSET));
+	                    
+	                    if(item.getFieldName().equals("title")) {
+	                    	title = item.getString(CHARSET);
+	                    }else if(item.getFieldName().equals("content")) {
+	                    	content = item.getString(CHARSET);
+	                    }
+	                } else {
+	                    System.out.printf("파라미터 명 : %s, 파일 명 : %s,  파일 크기 : %s bytes \n", item.getFieldName(),
+	                            item.getName(), item.getSize());
+	                    if (item.getSize() > 0) {
+	                        String separator = File.separator;
+	                        int index =  item.getName().lastIndexOf(separator);
+	                        file[i] = item.getName().substring(index  + 1);
+	                        System.out.println("index : "+ i +" fileName " +file[i]);
+	                        File uploadFile = new File(ATTACHES_DIR +  separator + file[i]);
+	                        if(uploadFile.exists()) { // 중복된 파일명이 존재할 경우
+	                        	boolean check = true;
+	                        	
+	                        	int j = 0;
+	                        	while(check) {
+	                        		j++;
+	                        		uploadFile = new File(ATTACHES_DIR +  separator + file[i] + "(" + j + ")");
+	                        		if(!uploadFile.exists()) {
+	                        			break;
+	                        		}
+	                        	}
+	                        }
+	                        item.write(uploadFile);
+	                        i++;
+	                    }
+	                }
+	            }
+	        } catch (Exception e) {
+	            // 파일 업로드 처리 중 오류가 발생하는 경우
+	        	System.out.println("에러");
+	            e.printStackTrace();
+	        }
+
+	        
+	        
+			UserVo authUser = getAuthUser(request);
 			
 			int userNo = authUser.getNo();
 			System.out.println("userNo : ["+userNo+"]");
 			System.out.println("title : ["+title+"]");
 			System.out.println("content : ["+content+"]");
+			System.out.println("file1 : ["+file[0]+"]");
+			System.out.println("file2 : ["+file[1]+"]");
 
-			BoardVo vo = new BoardVo(title, content, userNo);
-			BoardDao dao = new BoardDao();
+			BoardVo vo = new BoardVo(userNo, title, content, file[0], file[1]);
+			BoardDao dao = new BoardDaoImpl();
 			dao.insert(vo);
 
 			WebUtil.redirect(request, response, "/mysite/board?a=list");
@@ -96,10 +216,57 @@ public class BoardServlet extends HttpServlet {
 		} else if ("delete".equals(actionName)) {
 			int no = Integer.parseInt(request.getParameter("no"));
 
-			BoardDao dao = new BoardDao();
+			BoardDao dao = new BoardDaoImpl();
 			dao.delete(no);
 
 			WebUtil.redirect(request, response, "/mysite/board?a=list");
+
+		} else if ("search".equals(actionName)) {
+			BoardDao dao = new BoardDaoImpl();
+			List<BoardVo> list = dao.search(request.getParameter("kwd"));
+
+			System.out.println(list.toString());
+
+			// 리스트 화면에 보내기
+			request.setAttribute("list", list);
+			WebUtil.forward(request, response, "/WEB-INF/views/board/list.jsp");
+		} else if ("download".equals(actionName)) { 
+			String fileName = request.getParameter("fileName");
+			System.out.println(ATTACHES_DIR + File.separator + fileName);
+			File file = new File(ATTACHES_DIR + File.separator + fileName);
+			if(file.exists()) {
+				OutputStream os = null;
+				FileInputStream is = null;
+				String encodedName = null;
+				if(request.getHeader("User-Agent").contains("Firefox")) {
+					encodedName = new String(fileName.getBytes("utf-8"),"ios-8859-1");
+				}
+				else {
+					encodedName = URLEncoder.encode(fileName,"utf-8");
+					encodedName = encodedName.replaceAll("\\+"," ");
+				}
+				response.setContentType("application/octet-stream");
+				response.setHeader("Content-Dispostion", "attachment;filename=" + fileName + ";");
+				try {
+					os = response.getOutputStream();
+					is = new FileInputStream(file);
+					int su = 0;
+					while((su = is.read()) != -1) {
+						os.write(su);
+					}
+					System.out.println("다운");
+				}catch(IOException e) {
+					System.out.println("에러!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+					e.printStackTrace();
+				}finally {
+					try {
+						if(os != null) os.close();
+						if(is != null) is.close();
+					}catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 
 		} else {
 			WebUtil.redirect(request, response, "/mysite/board?a=list");
--- mysite/src/main/webapp/WEB-INF/views/guestbook/list.jsp
+++ mysite/src/main/webapp/WEB-INF/views/guestbook/list.jsp
@@ -1,63 +1,31 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<%@page import="com.javaex.vo.GuestbookVo"%>
-<%@page import="java.util.List"%>
-<%@page import="java.util.ArrayList"%>
-
-<%
-    List<GuestbookVo> list = (List<GuestbookVo>)request.getAttribute("list");
-%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%> 
+<% pageContext.setAttribute( "newLine", "\n" ); %>
 
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link href="/mysite/assets/css/guestbook.css" rel="stylesheet"
-	type="text/css">
-<title>Insert title here</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link href="/mysite/assets/css/guestbook.css" rel="stylesheet" type="text/css">
+<title>Mysite</title>
 </head>
 <body>
-
 	<div id="container">
-
-		<div id="header">
-			<h1>MySite</h1>
-			<ul>
-				<!-- 로그인 전 -->
-				<li><a href="">로그인</a></li>
-				<li><a href="">회원가입</a></li>
-
-				<!-- 로그인 후 -->
-				<!-- 
-				<li><a href="">회원정보수정</a></li>
-				<li><a href="">로그아웃</a></li> 
-				<li> 홍길동님 안녕하세요^^;</li>
-				-->
-			</ul>
-		</div>
-		<!-- /header -->
-
-		<div id="navigation">
-			<ul>
-				<li><a href="">정종욱</a></li>
-				<li><a href="">방명록</a></li>
-				<li><a href="">게시판</a></li>
-			</ul>
-		</div>
-		<!-- /navigation -->
-
+		
+		<c:import url="/WEB-INF/views/includes/header.jsp"></c:import>
+		<c:import url="/WEB-INF/views/includes/navigation.jsp"></c:import>
+		
 		<div id="wrapper">
 			<div id="content">
 				<div id="guestbook">
-
-					<form action="/mysite/guestbook" method="post">
-
+					
+					<form method="post" action="/mysite/gb" >
+						<input type="hidden" name="a" value="add">
 						<table>
 							<tr>
-								<td>이름</td>
-								<td><input type="text" name="name" /></td>
-								<td>비밀번호</td>
-								<td><input type="password" name="password" /></td>
+								<td>이름</td><td><input type="text" name="name" /></td>
+								<td>비밀번호</td><td><input type="password" name="password" /></td>
 							</tr>
 							<tr>
 								<td colspan=4><textarea name="content" id="content"></textarea></td>
@@ -66,42 +34,37 @@
 								<td colspan=4 align=right><input type="submit" VALUE=" 확인 " /></td>
 							</tr>
 						</table>
+						
 					</form>
 					<ul>
-						<li>
-							<% if(list != null){
-    							for(GuestbookVo vo : list){ %>
-								<table width="510" border="1">
+						
+						<c:forEach items="${list}" var="vo">
+							<li>
+								<table>
 									<tr>
-										<td>[<%= vo.getNo() %>]</td>
-										<td><%= vo.getName() %></td>
-										<td><%= vo.getRegDate() %></td>
-										<td><a
-											href="/hellojsp/views?a=deleteform&no=<%=vo.getNo()%>&dbpass=<%=vo.getPassword()%>">삭제</a></td>
+										<td>[${vo.no }]</td>
+										<td>${vo.name }</td>
+										<td>${vo.regDate }</td>
+										<td><a href="/mysite/gb?a=deleteform&no=${vo.no }">삭제</a></td>
 									</tr>
 									<tr>
-										<td colspan=4><%= vo.getContent() %></td>
+										<td colspan=4>
+										${fn:replace(vo.content, newLine, "<br>")}
+										</td>
 									</tr>
-								</table> <br> 
-							<% } 
-    						} %>
-						</li>
+								</table>
+								<br>
+							</li>
+						</c:forEach>
 					</ul>
-
-				</div>
-				<!-- /guestbook -->
-			</div>
-			<!-- /content -->
-		</div>
-		<!-- /wrapper -->
-
-		<div id="footer">
-			<p>(c)opyright 2015,2016,2017</p>
-		</div>
-		<!-- /footer -->
-
-	</div>
-	<!-- /container -->
-
+					
+				</div><!-- /guestbook -->
+			</div><!-- /content -->
+		</div><!-- /wrapper -->
+		
+		<c:import url="/WEB-INF/views/includes/footer.jsp"></c:import>
+		
+	</div><!-- /container -->
 </body>
-</html>
+</html>		
+		
