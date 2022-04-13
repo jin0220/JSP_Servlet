5dd76ecee76615c4f16098226adeea5516de7be1
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-04-13 21:58:15 +0900

    리팩토링: mvvm, retrofit 사용

---
+++ mysite/src/main/java/com/javaex/dao/BoardDao.java
@@ -0,0 +1,15 @@
+package com.javaex.dao;
+
+import java.util.List;
+import com.javaex.vo.BoardVo;
+
+public interface BoardDao {
+	public List<BoardVo> getList();  // 게시물 전체 목록 조회
+	public BoardVo getBoard(int no); // 게시물 상세 조회
+	public int insert(BoardVo vo);   // 게시물 등록
+	public int delete(int no);       // 게시물 삭제
+	public int update(BoardVo vo);   // 게시물 수정
+	public int update_hit(BoardVo vo); // 게시물 조회수
+	public List<BoardVo> search(String kwd); // 게시물 검색
+	public List<BoardVo> getBoardList(int startRow, int pageSize); // 페이징 처리
+}
