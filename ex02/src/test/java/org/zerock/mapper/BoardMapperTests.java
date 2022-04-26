5b2105867591d15ef74e1b5c9d5e93391b26ad5a
jin0220 <37447282+jin0220@users.noreply.github.com>
2022-04-26 17:19:05 +0900

    view 연동

--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,5 +1,6 @@
 package org.zerock.mapper;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +19,14 @@ public class BoardMapperTests {
 	@Setter(onMethod =  @__({ @Autowired }))
 	private BoardMapper mapper;
 
+//	@Ignore
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
 	}
 	
-	@Test
+//	@Test
+	@Ignore
 	public void testinsert() {
 		BoardVO board = new BoardVO();
 		board.setTitle("새로 작성하는 글");
@@ -34,6 +37,30 @@ public class BoardMapperTests {
 		
 		log.info(board);
 		
+	}
+	
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(5L);
+		log.info(board);
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info("DELETE COUNT: " + mapper.delete(3L));
+	}
+	
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int count = mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
 	}
 
 }
